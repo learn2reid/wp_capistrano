fdf33e4b122c32a2018c2c39a1316a63f3ee71bf
Lorelle Reid <lorelle.reid@gmail.com>
2015-01-11 17:58:20 -0500

    Updated Readme, WP version, Tutorials, RB scripts

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,18 +1,19 @@
 # Set the deployment directory on the target hosts.
-set :deploy_to, "/home/username/sites/#{application}-#{stage}" #FIXME
+set :deploy_to, "/home/username/sites/#{application}-#{stage}" #FIXME Ex) /home2/thegifx1/sites/#{application}-#{stage}
 
 # Use the correct branch on github. Uncomment this if you have set up seperate branches for each staging area
 set :branch, "dev" #FIXME
 
 # The hostnames to deploy to.
-role :web, "yourdomain.com" #FIXME
+role :web, "yourdomain.com" #FIXME Ex) thegiftedprogramnyc.com
 
 # Specify one of the web servers to use for database backups or updates.
 # This server should also be running Wordpress.
-role :db, "yourdomain.com", :primary => true #FIXME
+role :db, "yourdomain.com", :primary => true #FIXME Ex) thegiftedprogramnyc.com
 
 # The path to wp-cli
 set :wp, "cd #{current_path}/#{app_root} ; /usr/bin/wp" #FIXME (Should be the full path to the wp command on your server)
+# Ex) "cd #{current_path}/#{app_root} ; /home2/thegifx1/wp"
 
 # The username on the target system, if different from your local username
-ssh_options[:user] = 'username' #FIXME
+ssh_options[:user] = 'username' # Ex) thegifx1
